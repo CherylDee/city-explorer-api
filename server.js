695a536554303f967e362dfd48ccebeed7c36f70
Cheryl Dee <cdmccalop@gmail.com>
2023-03-12 13:16:15 -0700

    server done

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ require('dotenv').config();
 const cors = require('cors');
 
 
+const data = require('./data/weather.json');
+const { response } = require('express');
+
+
 // **** app is the server *****
 const app = express();
 
@@ -20,9 +24,48 @@ app.use(cors());
 const PORT = process.env.PORT || 3002;
 
 
-//****** endpoints that use GET / method that use a callback with 2 arg = req, res *******/
-app.get('/', (request, response) => {
-  response.status(200).send('welcome user!');
+//****** TRY endpoints that use GET / method that use a callback with 2 arg = req, res *******/
+app.get('/', (req, res) => {
+  res.status(200).send('Welcome to our server');
+});
+
+//**** define weather endpoints ****/
+app.get('/', (req, res, next) => {
+
+  // **** accept search queries - lat, lon, searchQuery - request.query /weather?lat, lon, query=value
+  try {
+    // let lat = req.query.lat;
+    // let lon = res.query.lon;
+    let cityName = req.query.searchQuery;
+    let city = data.find(city => city.city_name.toLowerCase() === cityName.toLowerCase());
+
+    let weatherData = city.data.map(dayObj => new Forecast(dayObj));
+
+    // *** verify working data from weather data ****
+    response.status(200).send(weatherData);
+
+  } catch(error) {
+    next(error);
+  }
+});
+
+// *** groom data for forecast class ****
+class Forecast {
+  constructor(dayObj) {
+    this.date = dayObj.valid_date;
+    this.description = dayObj.weather.description;
+  }
+}
+
+// *** Catch all should be the last defined endpoint ****
+
+app.get('*', (request, response) => {
+  response.status(404).send('This page does not exist');
+});
+
+//***** error handling *****/
+app.use((error, request, response) => {
+  response.status(500).send(error.message);
 });
 
 //******start server******/
