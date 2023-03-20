fac060b2f0de1f00a41724cf0f3b0ed1f88d00a2
Cheryl Dee <cdmccalop@gmail.com>
2023-03-19 20:47:44 -0700

    updated files

--- server.js
+++ server.js
@@ -32,8 +32,6 @@ app.get('/movies', getMovies);
 
 
 
-
-
 // ****** CATCH ALL ENDPOINT - NEEDS TO BE LAST DEFINED ENDPOINT
 
 app.get('*', (request, response) => {
@@ -43,7 +41,16 @@ app.get('*', (request, response) => {
 
 // ***** ERROR HANDLING - PLUG AND PLAY CODE FROM EXPRESS DOCS
 app.use((error, request, response, next) => {
+  setTimeout(() => {
+    try {
+      throw new Error('BROKEN');
+    } catch (err) {
+      next(err);
+    }
+  }, 100);
+  console.error(error);
   response.status(500).send(error.message);
+
 });
 
 // *****Server Start
